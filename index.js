64c3739b4169ab76c80ce3736b2aa05658b11d59
Yevhen Kornilov <zhenyakornilov98@gmail.com>
2022-08-15 20:50:41 +0300

    change index.js

--- index.js
+++ index.js
@@ -8,6 +8,56 @@
 // ======== OBJECTS DEFINITIONS ========
 // Define your objects here
 
+const dog = {
+   species: "dog",
+   name: "Ronnie",
+   gender: "male",
+   paws: 4,
+   legs: 0,
+   hands: 0,
+   saying: "Woof!",
+};
+
+const cat = {
+   species: "cat",
+   name: "Reggie",
+   gender: "male",
+   paws: 4,
+   legs: 0,
+   hands: 0,
+   saying: "Meoooow!",
+};
+
+const woman = {
+   species: "human",
+   name: "Agnes",
+   gender: "female",
+   paws: 0,
+   legs: 2,
+   hands: 2,
+   saying: "Hi, my name is Agnes",
+};
+
+const man = {
+   species: "human",
+   name: "Mark",
+   gender: "male",
+   paws: 0,
+   legs: 2,
+   hands: 2,
+   saying: "Hello, I am Mark",
+   friends: [cat.name, dog.name],
+};
+
+const catWoman = {
+   species: "catwoman",
+   name: "Selina",
+   gender: "female",
+   paws: 0,
+   legs: 2,
+   hands: 2,
+   saying: cat.saying,
+};
 
 // ======== OUTPUT ========
 /* Use print(message) for output.
@@ -28,4 +78,7 @@
    print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
    */
 
-
+const inhabitants = [dog, cat, woman, man, catWoman];
+for (let inhabitant of inhabitants) {
+   print(Object.values(inhabitant).join("; "));
+}
