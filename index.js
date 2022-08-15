2c36badb16aa7c322c1459c7109d7f4b8901f65e
Yevhen Kornilov <zhenyakornilov98@gmail.com>
2022-08-15 22:20:32 +0300

    Complete first part

--- index.js
+++ index.js
@@ -9,54 +9,54 @@
 // Define your objects here
 
 const dog = {
-   species: "dog",
-   name: "Ronnie",
-   gender: "male",
-   paws: 4,
-   legs: 0,
-   hands: 0,
-   saying: "Woof!",
+  species: "dog",
+  name: "Ronnie",
+  gender: "male",
+  paws: 4,
+  legs: 0,
+  hands: 0,
+  saying: "Woof!",
 };
 
 const cat = {
-   species: "cat",
-   name: "Reggie",
-   gender: "male",
-   paws: 4,
-   legs: 0,
-   hands: 0,
-   saying: "Meoooow!",
+  species: "cat",
+  name: "Reggie",
+  gender: "male",
+  paws: 4,
+  legs: 0,
+  hands: 0,
+  saying: "Meoooow!",
 };
 
 const woman = {
-   species: "human",
-   name: "Agnes",
-   gender: "female",
-   paws: 0,
-   legs: 2,
-   hands: 2,
-   saying: "Hi, my name is Agnes",
+  species: "human",
+  name: "Agnes",
+  gender: "female",
+  paws: 0,
+  legs: 2,
+  hands: 2,
+  saying: "Hi, my name is Agnes",
 };
 
 const man = {
-   species: "human",
-   name: "Mark",
-   gender: "male",
-   paws: 0,
-   legs: 2,
-   hands: 2,
-   saying: "Hello, I am Mark",
-   friends: [cat.name, dog.name],
+  species: "human",
+  name: "Mark",
+  gender: "male",
+  paws: 0,
+  legs: 2,
+  hands: 2,
+  saying: `Hello, I am Mark`,
+  friends: [cat.name, dog.name],
 };
 
 const catWoman = {
-   species: "catwoman",
-   name: "Selina",
-   gender: "female",
-   paws: 0,
-   legs: 2,
-   hands: 2,
-   saying: cat.saying,
+  species: "catwoman",
+  name: "Selina",
+  gender: "female",
+  paws: 0,
+  legs: 2,
+  hands: 2,
+  saying: cat.saying,
 };
 
 // ======== OUTPUT ========
@@ -79,6 +79,17 @@ const catWoman = {
    */
 
 const inhabitants = [dog, cat, woman, man, catWoman];
+const properties = [
+  "species",
+  "name",
+  "gender",
+  "paws",
+  "legs",
+  "hands",
+  "saying",
+  "friends",
+];
+
 for (let inhabitant of inhabitants) {
-   print(Object.values(inhabitant).join("; "));
+  print(properties.map((key) => inhabitant[key]).join("; "));
 }
